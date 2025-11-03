38e6ebc1c4f1b4edd90c75f5a20ac7bfa683551e
DylhanK <dylhan1107@hotmail.com>
2025-11-03 01:43:22 +0000

    Add files via upload

---
+++ 0.1 Drawing/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(0, 0, 255)
+  circle(50, 50, 50);
+  circle(200, 50, 50);
+
+  // Mouth
+  fill(255, 0, 0,)
+  ellipse(150, 150, 200, 20);
+}
